98c249d2d21f17571d4c2b9b5dc962100327e6ac
PauliusTaraskevicius <fixas1029@gmail.com>
2025-09-23 10:46:25 +0300

    08-general info, personal info forms

--- src/app/(main)/editor/ResumeEditor.tsx
+++ src/app/(main)/editor/ResumeEditor.tsx
@@ -2,6 +2,8 @@
 
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { GeneralInfoForm } from "./forms/GeneralInfoForm";
+import { PersonalInfoForm } from "./forms/PersonalInfoForm";
 
 export const ResumeEditor = () => {
   return (
@@ -15,7 +17,9 @@ export const ResumeEditor = () => {
       </header>
       <main className="relative grow">
         <div className="absolute top-0 bottom-0 flex w-full">
-          <div className="w-full md:w-1/2">left</div>
+          <div className="w-full overflow-y-auto p-3 md:w-1/2">
+            <PersonalInfoForm />
+          </div>
           <div className="grow md:border-r" />
           <div className="hidden w-1/2 md:flex">right</div>
         </div>
