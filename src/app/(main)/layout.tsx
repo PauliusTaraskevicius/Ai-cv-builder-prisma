719d6f359dc0675881a6882cd85d11b1a49c938b
PauliusTaraskevicius <fixas1029@gmail.com>
2025-10-06 09:34:29 +0300

    27-save fetch subscriptions

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,13 +1,25 @@
 import { PremiumModal } from "@/components/premium/PremiumModal";
 import { Navbar } from "./Navbar";
+import { SubscriptionLevelProvider } from "./SubscriptionLevelProvider";
+import { auth } from "@clerk/nextjs/server";
+import { getUserSubscriptionLevel } from "@/lib/subscription";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = async ({ children }: { children: React.ReactNode }) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const userSubscriptionLevel = await getUserSubscriptionLevel(userId);
   return (
-    <div className="flex min-h-screen flex-col">
-      <Navbar />
-      {children}
-      <PremiumModal />
-    </div>
+    <SubscriptionLevelProvider userSubscriptionLevel={userSubscriptionLevel}>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        {children}
+        <PremiumModal />
+      </div>
+    </SubscriptionLevelProvider>
   );
 };
 
